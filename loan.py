bd8ec944d20d182a0055d7fa63bf5516030e8b85
Eternity1001 <mmirhossain01@gmail.com>
2024-11-27 12:27:49 -0500

    added the py file and the data set

---
+++ loan.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
+
+class ANN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
